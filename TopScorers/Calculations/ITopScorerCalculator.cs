ea365e241a57ea46c079a5f93facdae4f88cd297
Lorrainem <moyolorraine98@gmail.com>
2023-06-14 11:14:05 +0200

    Isolated people data validation

--- TopScorers/Calculations/ITopScorerCalculator.cs
+++ TopScorers/Calculations/ITopScorerCalculator.cs
@@ -2,6 +2,7 @@
 {
     public interface ITopScorerCalculator
     {
+        void ValidateData(Dictionary<string, int> people);
         List<string> GetTopScorers(Dictionary<string, int> people);
         int GetHighestScore(Dictionary<string, int> people);
     }
